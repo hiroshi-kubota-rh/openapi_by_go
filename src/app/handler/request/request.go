e0b116aa461c6fa5e5f73a36ac262abb46e9d166
hiroshi-kubota-rh <hiroshi_kubota@reazon.jp>
2022-01-28 11:58:54 +0900

    http requestの内容をvalidateする

---
+++ src/app/handler/request/request.go
@@ -0,0 +1,34 @@
+package request
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi"
+	"github.com/pkg/errors"
+)
+
+// Read path parameter `id`
+func IDOf(r *http.Request) (int64, error) {
+	ids := chi.URLParam(r, "id")
+
+	if ids == "" {
+		return -1, errors.Errorf("id was not presence")
+	}
+
+	id, err := strconv.ParseInt(ids, 10, 64)
+	if err != nil {
+		return -1, errors.Errorf("id was not number")
+	}
+
+	return id, nil
+}
+
+func UserNameOf(r *http.Request) (*string, error) {
+	username := chi.URLParam(r, "username")
+
+	if username == "" {
+		return nil, errors.Errorf("username was not presence")
+	}
+	return &username, nil
+}
